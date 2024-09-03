25a86bfa1dfb70c1ff20a1b7e69bfbc3c984bbb1
Samuel Hossain <samuel.hossain@outlook.com>
2024-09-03 11:55:37 -0400

    my LINQ exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LINQ
+﻿using System.Linq;
+//using LINQ;
+
+
+List<string> videoGames = new List<string>()
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    
+    "Doom Eternal",
+    "Super Mario Galaxy",
+    "Call of Duty Black Ops 2",
+    "Halo Master Chief Collection",
+    "Vanquish",
+    "Sunset Overdrive",
+    "Trepang2",
+    "Mario Kart",
+    "Ultrakill",
+    "Monster Hunter Rise"
+    
+};
+
+
+var orderedGames = videoGames.OrderBy(game => game.Length).ToList();
+
+
+Console.WriteLine("Here are my favorite video games in order: ");
+
+foreach (var game in orderedGames)
+{
+    Console.WriteLine(game);
 }
